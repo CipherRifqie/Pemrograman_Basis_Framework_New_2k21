ca6bbf620f29dad133c3d5b740e45729f2646bb9
CipherRifqie <rifqie.aries@gmail.com>
2021-04-22 22:38:08 +0700

    Upload tugas Minggu 10

---
+++ Minggu10/task11only/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyC4NV_ccy1I38MKIyXpNWSPO_FVf3jXADI",
+    authDomain: "cipherrifqie6th.firebaseapp.com",
+    databaseURL: "https://react-firebase-eb81b.firebaseio.com",
+    projectId: "cipherrifqie6th",
+    storageBucket: "cipherrifqie6th.appspot.com",
+    messagingSenderId: "228806967584",
+    appId: "1:228806967584:web:b5f1d2f2a633b69bfd1745",
+    measurementId: "G-VXJJYSY54M"
+  };
